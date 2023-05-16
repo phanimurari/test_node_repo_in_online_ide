96368d9d048f81438281c1d14c3e15569d9e51ce
Phani Murari <imphanimurari1@gmail.com>
2023-05-16 12:46:54 +0000

    intial commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const path = require("path");
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+
+const app = express();
+app.use(express.json());
+
+let db = null;
+const initializeDBAndServer = async () => {
+  try {
+    app.listen(3000, () => {
+      console.log("Server Running at http://localhost:3000/");
+    });
+  } catch (e) {
+    console.log(`DB Error : ${e.message}`);
+    process.exit(1);
+  }
+};
+initializeDBAndServer();
+
+// API 1
+
+app.get("/players/", async (request, response) => {
+  response.send("Hello World!");
+});
+
+module.exports = app;
